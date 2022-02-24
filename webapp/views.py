f9571084b26102230d86048eac06fc3fb2f64318
kwang38 <47671423+kwang38@users.noreply.github.com>
2022-02-24 05:26:01 -0500

    #13 Implemented valid UB email check

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,24 @@
 from webapp import app
+from flask import request, render_template
+from webapp.models import Accounts
 
 
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello World!'
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    msg = ""
+    if request.method == 'POST':
+        email = request.form['email'].strip().lower()
+        password = request.form['password']
+        email_query = Accounts.query.filter_by(email=email).first()
+        if email_query:
+            msg = "Email In Use"
+        elif len(email.split('@')) == 2 and len(email.split('.')) == 2 and "@buffalo.edu" in email:
+            msg = "Valid UB Email"
+        else:
+            msg = "Invalid UB Email"
+    return render_template('signup.html', msg=msg)
